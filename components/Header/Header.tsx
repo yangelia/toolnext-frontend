722ca73b3e79a58f990c26b1fd1ff62c4b0b5121
Maryna Tkachenko <marynapanchenko19@gmail.com>
2025-12-14 10:31:02 +0200

    test

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import css from "./Header.module.css";
+
 export default function Header() {
-  return <header>{/* TODO: Add navigation */}</header>;
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <Link href="/" className={css.logo} aria-label="ToolNext home">
+          <Image
+            src="/images/Tool-Next.svg"
+            alt="ToolNext"
+            width={124}
+            height={20}
+            priority
+          />
+        </Link>
+
+        <button className={css.menuBtn} type="button" aria-label="Open menu">
+          <svg
+            className={css.menuIcon}
+            width="24"
+            height="24"
+            aria-hidden="true"
+          >
+            <use href="/icons/sprite.svg#icon-menu" />
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
 }
