1bfcf55fc42a6ddfa9fbcab321e1723698a01703
Katerynakulik <katerynakulik4@gmail.com>
2025-12-16 11:00:12 +0200

    First auth structure

--- app/(auth routes)/register/page.tsx
+++ app/(auth routes)/register/page.tsx
@@ -1,10 +1,52 @@
-import styles from "./RegisterPage.module.css";
+"use client";
+import { register, RegisterRequest } from "@/lib/api/clientApi";
+import css from "./RegisterPage.module.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function RegisterPage() {
+  const handleSubmit = async (formData: FormData) => {
+    try {
+      const payload = Object.fromEntries(formData) as RegisterRequest;
+      const data = await register(payload);
+      if (data) {
+        setUser(data);
+        toast.success("Registration successful!");
+        router.push("/profile");
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      setError(
+        (error as ApiError).response?.data?.error ??
+          (error as ApiError).message ??
+          "Oops... some error"
+      );
+      toast.error("Oops... some error");
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <h1>Register</h1>
-      {/* Тут позже буде форма реєстрації */}
-    </div>
+    <main className={css.mainContent}>
+      <h1 className={css.formTitle}>Реєстрація</h1>
+      <form className={css.form} action="handleSubmit">
+        <label>
+          Ім'я*
+          <input type="text" name="userName" className={css.input} />
+        </label>
+        <label>
+          Пошта*
+          <input type="email" name="email" className={css.input} />
+        </label>
+        <label>
+          Пароль*
+          <input type="password" name="password" className={css.input} />
+        </label>
+        <label>
+          Підтвердіть пароль*
+          <input type="password" name="password" className={css.input} />
+        </label>
+      </form>
+    </main>
   );
 }
--- app/api/api.ts
+++ app/api/api.ts
@@ -1,6 +1,7 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
+export type ApiError = AxiosError<{ error: string }>;
