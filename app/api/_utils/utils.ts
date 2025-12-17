a5b3efe914d666da9eb74f792053c91ebd8702c7
yangelia <yana.sychova@gmail.com>
2025-12-17 13:43:38 +0200

    temp commit

--- app/api/_utils/utils.ts
+++ app/api/_utils/utils.ts
@@ -1,9 +1,5 @@
 export function logErrorResponse(errorObj: unknown): void {
-  const green = "\x1b[32m";
-  const yellow = "\x1b[33m";
-  const reset = "\x1b[0m";
-
-  // Стрелка зелёная, текст жёлтый
-  console.log(`${green}> ${yellow}Error Response Data:${reset}`);
-  console.dir(errorObj, { depth: null, colors: true });
+  console.log("API error response:");
+  console.dir(errorObj, { depth: null });
 }
+// це лог, не devtool, кольори приберемо, спростимо код
