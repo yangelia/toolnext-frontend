0b9de61c61589fcccaf387d1c50b4c861a3ae6b3
yangelia <yana.sychova@gmail.com>
2025-12-24 12:05:54 +0200

    upt page

--- app/(public)/tools/[id]/page.tsx
+++ app/(public)/tools/[id]/page.tsx
@@ -1,7 +1,36 @@
-export default function ToolDetailsPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  return <div>Tool details: {params.id}</div>;
+import { notFound } from "next/navigation";
+
+import ToolGallery from "@/components/ToolGallery/ToolGallery";
+import ToolInfoBlock from "@/components/ToolInfoBlock/ToolInfoBlock";
+
+import { getToolId } from "@/lib/api/booking";
+import { ToolDetails } from "@/types/tool";
+
+import styles from "./toolDetails.module.css";
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function ToolDetailsPage({ params }: PageProps) {
+  let tool: ToolDetails;
+
+  try {
+    tool = await getToolId(params.id);
+  } catch {
+    notFound();
+  }
+
+  if (!tool) {
+    notFound();
+  }
+
+  return (
+    <section className={`${styles.page} container`}>
+      <ToolGallery images={tool.images} />
+      <ToolInfoBlock tool={tool} />
+    </section>
+  );
 }
