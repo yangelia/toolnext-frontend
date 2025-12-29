854206e45a5c49e0a9872eb6ce16eff8168aba11
yangelia <yana.sychova@gmail.com>
2025-12-29 14:23:38 +0200

    upd api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,15 +1,12 @@
-// lib\api\api.ts
-
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000";
 
 export const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true, // cookies-based auth
+  withCredentials: true,
 });
 
-// Optional error handling
 api.interceptors.response.use(
   (response) => response,
   (error) => {
